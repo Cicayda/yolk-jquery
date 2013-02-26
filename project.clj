548a3db96247d3ed73842f7affbd0d0616e9b908
Wilkes Joiner <wilkesjoiner@gmail.com>
2013-02-26 12:24:41 -0600

    update yolk dep

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [jayq "2.2.0"]
-                 [yolk "0.1.0-SNAPSHOT"]]
+                 [yolk "0.1.0"]]
   :plugins [[lein-cljsbuild "0.3.0"]]
   :cljsbuild {:builds
               {:main
