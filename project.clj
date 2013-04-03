05ca8aca9676df02a9f32fe91d5762d58c0d81fa
Wilkes Joiner <wilkesjoiner@gmail.com>
2013-04-03 13:12:15 -0500

    added class-if, show, hide, show-when, hide-when, switch-when

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject yolk-jquery "0.2.0-SNAPSHOT"
+(defproject yolk-jquery "0.3.0"
   :description "Bacon and Eggs with jQuery"
   :url "https://github.com/cicayda/yolk-jquery"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [jayq "2.2.0"]
-                 [yolk "0.2.0-SNAPSHOT"]]
+                 [jayq "2.3.0"]
+                 [yolk "0.5.0"]]
   :plugins [[lein-cljsbuild "0.3.0"]]
   :cljsbuild {:builds
               {:main
--- src/yolk/ui.cljs
+++ src/yolk/ui.cljs
@@ -38,14 +38,41 @@
       b/to-property
       b/skip-duplicates))
 
-(defn disabled [$elem property]
+(defn class-if [$elem klass property]
   (b/on-value property
               (fn [x?]
-                ((if x? j/add-class j/remove-class)
-                 $elem "disabled"))))
+                ((if x?  j/add-class j/remove-class)
+                 $elem klass))))
 
-(defn enabled [$elem property]
-  (b/on-value property
-              (fn [x?]
-                ((if x? j/remove-class j/add-class)
-                 $elem "disabled"))))
+(defn show
+  ([$elem property]
+     (show $elem property nil))
+  ([$elem options property]
+     (b/on-value (b/filter property) #(.show $elem options))))
+
+(defn hide
+  ([$elem property]
+     (hide $elem property nil))
+  ([$elem options property]
+     (b/on-value (b/filter property) #(.hide $elem options))))
+
+(defn show-when
+  ([$elem property]
+     (show-when $elem property))
+  ([$elem options property]
+     (show $elem options property)
+     (hide $elem options (b/not property))))
+
+(defn hide-when
+  ([$elem property]
+       (hide-when $elem property))
+  ([$elem options property]
+     (hide $elem options property)
+     (show $elem options (b/not property))))
+
+(defn switch-when
+  ([$elem1 $elem2 property]
+     (switch-when $elem1 $elem2 nil property))
+  ([$elem1 $elem2 options property]
+     (show-when $elem1 options property)
+     (hide-when $elem2 options property)))
