c8c264efa7cbbc1e122740b828b30d42a2623b36
Seth Buntin <seth.buntin@gmail.com>
2013-05-26 08:38:02 -0500

    add remote polling functionality

--- src/yolk/net.cljs
+++ src/yolk/net.cljs
@@ -11,6 +11,20 @@
          :data {:remote name
                 :params (pr-str (vec params))}}))
 
+(defn remote-poll [bus timeout name params]
+  (->  (j/ajax {:url "_fetch"
+                :type "POST"
+                :data {:remote name
+                       :params (pr-str (vec params))}})
+       (.always (fn [e]
+                  (b/push bus e)
+                  (js/setTimeout #(remote-poll bus timeout name params) timeout)))))
+
+(defn remote-long-poll [timeout remote & args]
+  (let [read-bus (b/bus)]
+    (js/setTimeout #(remote-poll read-bus timeout remote args) 1)
+    read-bus))
+
 (defn on-remote [name params f]
   (-> (apply remote name params)
       (b/on-value f)))
