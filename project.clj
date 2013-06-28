946a86183b8fd1e94c2957ddf30d637dbfbb3a92
Wilkes Joiner <wilkesjoiner@gmail.com>
2013-06-28 11:22:31 -0500

    bump to 0.5.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yolk-jquery "0.4.0"
+(defproject yolk-jquery "0.5.0"
   :description "Bacon and Eggs with jQuery"
   :url "https://github.com/cicayda/yolk-jquery"
   :license {:name "Eclipse Public License"
@@ -6,7 +6,7 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [jayq "2.3.0"]
-                 [yolk "0.6.0"]]
+                 [yolk "0.7.0"]]
   :plugins [[lein-cljsbuild "0.3.0"]]
   :cljsbuild {:builds
               {:main
